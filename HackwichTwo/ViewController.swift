3093ebcdab55b7f07609f2b49887fd1611e3fd97
Tani Umetsu <taniu@hawaii.edu>
2023-09-06 09:55:20 -1000

    Hackwich 2 Completed

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -8,19 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Part 8
+    var myFirstVariable: String = "Hi my name is"
+    
+    var mySecondVariable: String = " Tani"
+    
+    var myThirdVariable: String = ""
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myThirdVariable = myFirstVariable + mySecondVariable
+        
+        
     }
-
+    @IBAction func pressMeButton(_ sender: Any)
+    {
+        print(myThirdVariable)
+    }
+    
 
     @IBAction func pressMeButtonPressed(_ sender: Any)
     {
         print("Hello World")
         
     }
-    
     
     
 }
